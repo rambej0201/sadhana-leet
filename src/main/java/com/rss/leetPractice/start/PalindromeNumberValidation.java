947b646dd7f6d1c41d3cab9a93c0e6e8160cd0ef
rambej0201 <1503010201@svuca.edu>
2023-01-18 14:34:50 -0800

    palindromeNumber commit

--- src/main/java/com/rss/leetPractice/start/PalindromeNumberValidation.java
+++ src/main/java/com/rss/leetPractice/start/PalindromeNumberValidation.java
@@ -2,4 +2,28 @@ package com.rss.leetPractice.start;
 
 public class PalindromeNumberValidation {
 
+    public static void main(String[] args) {
+        int x = 121;
+        System.out.println(findPalindrome(121));
+
+    }
+
+
+    public static boolean findPalindrome(int xx){
+        if (xx < 0 || ( xx != 0 && xx % 10 == 0)){
+            return false;
+        }
+        int temp = xx;
+        int rev = 0;
+
+        while (xx > 0){
+            int digit = xx % 10;
+            rev = (rev * 10) + digit;
+            xx = xx/10;
+        }
+        if (rev == temp){
+            return true;
+        }
+        return false;
+    }
 }
